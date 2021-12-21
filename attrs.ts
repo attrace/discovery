ed080f3e13c8135cc154bb43e43e79fa351d5c72
ruleking <ruleking@gmail.com>
2021-12-21 13:04:08 +0100

    add BSC address

--- attrs.ts
+++ attrs.ts
@@ -16,5 +16,6 @@ const rows: NetworkContract[] = [
   { chainId: 137, address: '0x0335DeFC8A1977b99488e0b5f5c6bc3D44fAcdD4' },
   { chainId: 4, address: '0x926362b451A012F72b34240F36C3bDc163d462e0' },
   { chainId: 80001, address: '0xd148c38e3eB133CF32E2B65528540D9684De254d' },
+  { chainId: 56, address: '0x0f07ffd690dffd9a0ce8274116830cf62d086b5f' },
 ];
 
