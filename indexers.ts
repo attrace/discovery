5171d1f2869070a81a1b77ea7f68cc6fd2a28b43
Roel Berger <berger.roel@gmail.com>
2022-07-13 13:31:33 +0200

    disable jfk indexer

--- indexers.ts
+++ indexers.ts
@@ -12,6 +12,6 @@ export async function getIndexers(): Promise<IndexerInfo[]> {
 // Actual definitions without checks
 const indexers: IndexerInfo[] = [
   { url: 'https://indexer.attrace.com', location: { lat: 53.3331, lon: -6.2489 } },
-  { url: 'https://indexer-jfk.attrace.com', location: { lat: 40.639801, lon: -73.7789 } },
+  // { url: 'https://indexer-jfk.attrace.com', location: { lat: 40.639801, lon: -73.7789 } },
   { url: 'https://indexer-sin.attrace.com', location: { lat: 1.35019, lon: 103.994003 } },
 ];
